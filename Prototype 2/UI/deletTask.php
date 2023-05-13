2199fc9a498c128d95b7e437f7f1475c98e5b1ad
ilias anouar <ilas.anouar.solicode@gmail.com>
2023-05-13 17:30:12 +0100

    fixing errors in the delete and edit task files

--- Prototype 2/UI/deletTask.php
+++ Prototype 2/UI/deletTask.php
@@ -1,22 +1,18 @@
 <?php
 
-include "../Managers/GestionProject.php";
 if (file_exists('./managers/GestionTask.php')) {
-	include './managers/GestionTask.php';
+    include './managers/GestionTask.php';
 } elseif (file_exists('../managers/GestionTask.php')) {
-	include '../managers/GestionTask.php';
+    include '../managers/GestionTask.php';
 } else {
-	// Neither file exists, so handle the error here
-	echo "Error: Project.php not found in either directory.";
+    // Neither file exists, so handle the error here
+    echo "Error: Project.php not found in either directory.";
 }
+$id_projet = $_GET['Id_projet'];
+if (isset($_GET['Id_Task'])) {
 
-if(isset($_GET['Id_Task'])){
-
-    // Trouver tous les employés depuis la base de données 
-    $GestionTask= new GestionTask();
-    $id = $_GET['Id_Task'] ;
+    $GestionTask = new GestionTask();
+    $id = $_GET['Id_Task'];
     $GestionTask->Supprimer($id);
-        
-    header("Location: tasks.php?id=".$id);
+    header("Location: tasks.php?id=" . $id_projet);
 }
-
