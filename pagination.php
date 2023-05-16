58fc1efa31397f641bed7058b5fe887a036ed842
ilias anouar <ilas.anouar.solicode@gmail.com>
2023-05-16 22:15:56 +0100

    Update pagination.php

--- pagination.php
+++ pagination.php
@@ -41,10 +41,6 @@ $books = $books->fetchAll(PDO::FETCH_ASSOC);
 
 
 
-
-
-
-<?php
 $pageId = isset($_GET['pageId']) ? $_GET['pageId'] : 1;
 
 $itemsPerPage = 6; 
@@ -66,4 +62,47 @@ if ($result = $conn->query($sql)) {
     }
     $result->free();
 }
+
+
+// exemple pagination
+public function Pagination($pageId)
+{
+    $endIndex = $pageId * 6;
+    $StartIndex = $endIndex - 6;
+    $sql = ("SELECT * FROM `projects` LIMIT 6 OFFSET $StartIndex");
+    // return $sql;
+    $projects = $this->RechercherTous($sql);
+    return $projects;
+}
+
+public function Page_num()
+{
+    $pagesNum = 0;
+    $page = 'SELECT * FROM projects';
+    $Projects_lentgh = mysqli_query($this->getConnection(), $page)->num_rows;
+    if (($Projects_lentgh % 6) == 0) {
+        $pagesNum = $Projects_lentgh / 6;
+    } else {
+        $pagesNum = ceil($Projects_lentgh / 6);
+    }
+    return $pagesNum;
+}
+
+public function RechercherTous($sql)
+{
+    $query = mysqli_query($this->getConnection(), $sql);
+    $projects_data = mysqli_fetch_all($query, MYSQLI_ASSOC);
+    $projects = array();
+    foreach ($projects_data as $project_data) {
+        $project = new Project();
+        $project->setId($project_data['Id']);
+        $project->setName($project_data['name']);
+        $project->setDescription($project_data['description']);
+        array_push($projects, $project);
+    }
+    return $projects;
+}
+
+
+
 ?>
